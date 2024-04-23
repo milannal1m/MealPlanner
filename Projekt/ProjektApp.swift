0ed4743d238f9b6f5c8438ce7c2a53d46e92ed31
MilanToth <milan.zweit@gmail.com>
2024-04-23 12:57:10 +0200

    Initial Commit

---
+++ Projekt/ProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjektApp.swift
+//  Projekt
+//
+//  Created by Milan Tóth on 23.04.24.
+//
+
+import SwiftUI
+
+@main
+struct ProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
